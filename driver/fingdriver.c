42f68ac1654ad828b28e327198a9332a913bf01b
GeraldSalazar <geraldsalazar4@gmail.com>
2023-05-29 18:48:36 -0600

    usb basic driver

--- driver/fingdriver.c
+++ driver/fingdriver.c
@@ -1,80 +1,83 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/fs.h>
 #include <linux/usb.h>
-#include <linux/tty.h>
-#include <linux/uaccess.h>
 
-#define FT232R_DEVICE_FILE "/dev/ttyUSB0"
+#define FT232R_VENDOR_ID 0x0403
+#define FT232R_PRODUCT_ID 0x6001
 
-static struct file* fingdriver_file;
-
-static void write_block(const char* block, size_t size)
-{
-    ssize_t bytes_written = vfs_write(fingdriver_file, block, size, &fingdriver_file->f_pos);
-
-    if (bytes_written < 0) {
-        printk(KERN_ERR "Failed to write to fingdriver\n");
-    } else if (bytes_written != size) {
-        printk(KERN_WARNING "Incomplete write to fingdriver\n");
-    }
-}
-
-static ssize_t write_char(struct file* file, const char __user* buffer, size_t count, loff_t* offset)
-{
-    char ch;
-    char block[4];
-
-    if (count != 1) {
-        return -EINVAL;
-    }
-
-    if (copy_from_user(&ch, buffer, sizeof(char)) != 0) {
-        return -EFAULT;
-    }
-
-    block[0] = ch;
-    block[1] = ch;
-    block[2] = ch;
-    block[3] = ch;
-
-    write_block(block, sizeof(block));
-
-    return sizeof(char);
-}
-
-static struct file_operations fingdriver_fops = {
-    .owner = THIS_MODULE,
-    .write = write_char,
+static struct usb_device_id fingdriver_table[] = {
+    { USB_DEVICE(FT232R_VENDOR_ID, FT232R_PRODUCT_ID) },
+    { } /* Terminating entry */
 };
+MODULE_DEVICE_TABLE(usb, fingdriver_table);
+
+static int fingdriver_probe(struct usb_interface *interface,
+                            const struct usb_device_id *id)
+{
+    struct usb_device *udev = interface_to_usbdev(interface);
+    struct usb_device_descriptor *dev_desc = &udev->descriptor;
+
+    printk(KERN_INFO "FT232R device plugged in: Vendor ID=0x%04X, Product ID=0x%04X\n",
+           dev_desc->idVendor, dev_desc->idProduct);
+
+    /* Perform necessary initialization and communication with the FT232R device */
+
+    return 0; /* Success */
+}
+
+static void fingdriver_disconnect(struct usb_interface *interface)
+{
+    struct usb_device *udev = interface_to_usbdev(interface);
+    struct usb_device_descriptor *dev_desc = &udev->descriptor;
+
+    printk(KERN_INFO "FT232R device unplugged: Vendor ID=0x%04X, Product ID=0x%04X\n",
+           dev_desc->idVendor, dev_desc->idProduct);
+
+    /* Perform necessary cleanup and release of resources */
+}
+
+static ssize_t fingdriver_write(struct file *filep, const char *buffer, size_t len, loff_t *offset)
+{
+    
+    return len; /* Return the number of bytes written */
+}
+
+static ssize_t fingdriver_read(struct file *filep, char *buffer, size_t len, loff_t *offset)
+{
+    
+    return len; /* Return the number of bytes read */
+}
+
+static struct usb_driver fingdriver_driver = {
+    .name = "fingdriver",
+    .id_table = fingdriver_table,
+    .probe = fingdriver_probe,
+    .disconnect = fingdriver_disconnect,
+};
+
+static int fingdriver_major = 47; /* Major number for the driver */
 
 static int __init fingdriver_init(void)
 {
-    fingdriver_file = filp_open(FT232R_DEVICE_FILE, O_WRONLY, 0);
-    if (IS_ERR(fingdriver_file)) {
-        printk(KERN_ERR "Failed to open fingdriver device file\n");
-        return PTR_ERR(fingdriver_file);
+    int result;
+
+    /* Register the USB driver */
+    result = usb_register(&fingdriver_driver);
+    if (result < 0) {
+        printk(KERN_ERR "Failed to register FT232R USB driver: %d\n", result);
+        return result;
     }
 
-    if (!fingdriver_file->f_op->write) {
-        filp_close(fingdriver_file, NULL);
-        printk(KERN_ERR "fingdriver does not support write operation\n");
-        return -EINVAL;
-    }
-
-    fingdriver_file->f_op = &fingdriver_fops;
-
-    printk(KERN_INFO "fingdriver initialized!!!!! \n");
+    printk(KERN_INFO "FT232R USB driver registered with major number %d\n", fingdriver_major);
     return 0;
 }
 
 static void __exit fingdriver_exit(void)
 {
-    if (fingdriver_file) {
-        filp_close(fingdriver_file, NULL);
-    }
+    /* Unregister the USB driver */
+    usb_deregister(&fingdriver_driver);
 
-    printk(KERN_INFO "fingdriver exited\n");
+    printk(KERN_INFO "FT232R USB driver unregistered\n");
 }
 
 module_init(fingdriver_init);
@@ -82,4 +85,4 @@ module_exit(fingdriver_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Your Name");
-MODULE_DESCRIPTION("fingdriver");
+MODULE_DESCRIPTION("FT232R USB Driver");
